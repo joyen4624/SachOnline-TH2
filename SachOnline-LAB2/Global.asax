5cce3d9a4ddcebbef6777e6db879b5bf911e48a3
joyen4624 <joyen4624@gmail.com>
2023-09-28 21:45:47 +0700

    Add project files.

---
+++ SachOnline-LAB2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SachOnline_LAB2.MvcApplication" Language="C#" %>
---
+++ SachOnline-LAB2/Models/ListNXB.cs
@@ -0,0 +1,18 @@
+﻿using SachOnline_LAB2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SachOnline.Models
+{
+    public class ListNXB
+    {
+        public List<NHAXUATBAN> LayDanhSachNhaXuatBan()
+        {
+            List<NHAXUATBAN> danhSachNhaXuatBan = new List<NHAXUATBAN>();
+
+            return danhSachNhaXuatBan;
+        }
+    }
+}
